ffb0d97619fbcbeafb44cfde5e7eb0225a06bfe6
vladislavis <vladislavis@is.com>
2021-10-19 14:31:41 +0500

    started

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { ConfigModule } from "@nestjs/config";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import * as path from "path";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, "static"),
+    }),
+    SequelizeModule.forRoot({
+      dialect: `postgres`,
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRESS_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRESS_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
